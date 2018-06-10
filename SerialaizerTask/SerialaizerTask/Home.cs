60ea564dee1524a71c53bb60a302c38d8a4b0e30
Ferahim91 <34597804+Ferahim91@users.noreply.github.com>
2018-06-10 04:40:41 +0400

    Serialaizer

---
+++ SerialaizerTask/SerialaizerTask/Home.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Reflection;
+
+namespace SerialaizerTask
+{
+    public class Home<T> where T:class
+    {
+        public string Serialize (T item)
+        {
+            PropertyInfo[] properties = item.GetType().GetProperties();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach(PropertyInfo a in properties)
+            {
+                stringBuilder.Append(String.Format("<p><span>{0}</span><span>{1}</span><span>{2}</span></p>", a.Name,a.GetValue(item)));
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ SerialaizerTask/SerialaizerTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialaizerTask
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public string Email { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                Id = 54,
+                Name = "Ferahim",
+                Age = 27,
+                Email = "farahimas@code.edu.az"
+            };
+            Home<Employee> home = new Home<Employee>();
+            home.Serialize(employee);
+            Console.WriteLine("SerialaizerTask");
+        }
+    }
+}
